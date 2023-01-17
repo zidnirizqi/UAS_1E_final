eff2ad3f1b2fdee4a034166d1865e25ac8f195cb
ACER <zidnirizqi80@gmail.com>
2023-01-17 12:03:05 +0700

    UAS_22090085_MOCH ZIDNI RIZQI RAMADHAN_1E

--- src/JAWABAN2.java
+++ src/JAWABAN2.java
@@ -1,2 +1,42 @@
-package PACKAGE_NAME;public class JAWABAN2 {
+import java.util.Arrays;
+
+public class JAWABAN2 {
+    public static String countPositivesSumNegativesSumNegatives(int[] input) {
+        int BilanganPositif = 0;
+        int BilanganNegatif = 0;
+
+        for (int i = 0; i <input.length; i++){
+
+            if (input[i] >0) {
+                BilanganPositif++;
+            }else if (input[i] < 0) {
+                BilanganNegatif +=input[i];
+            }
+        }
+        return Arrays.toString(new int[]{BilanganPositif,BilanganNegatif});
+    }
+    public static void main(String[] args) {
+        //Output {}
+        // System.out.println(new int[]{Integer.parseInt(null)});
+
+        //Output {}
+        //System.out.println(new int[]{});
+
+        //Output {10,-65}
+        System.out.println(countPositivesSumNegativesSumNegatives(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15}));
+
+        //Ouput {8,-50}
+        System.out.println(countPositivesSumNegativesSumNegatives(new int[]{0, 2, 3, 0, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14}));
+
+
+    }
+
+
 }
+
+
+
+
+
+
+
--- src/UAS1E.java
+++ src/UAS1E.java
@@ -1,9 +1,124 @@
-public class JAWABAN3 {
-    private String USERID;
-    private String USERNAME;
-    private String ;
+import java.util.ArrayList;
+import java.util.Scanner;
 
-}
+public class UAS1E {
+    private String USERID;
+
+    public String getUSERID() {
+        return USERID;
+    }
+
+    public void setUSERID(String USERID) {
+        this.USERID = USERID;
+    }
+
+    public String getUSERNAME() {
+        return USERNAME;
+    }
+
+    public void setUSERNAME(String USERNAME) {
+        this.USERNAME = USERNAME;
+    }
+
+    public String getFULLNAME() {
+        return WhatYouPosts;
+    }
+
+    public void setFULLNAME(String FULLNAME) {
+        this.WhatYouPosts = FULLNAME;
+    }
+
+    private String USERNAME;
+    private String WhatYouPosts;
+
+    public UAS1E(String userid, String username, String fullname, ArrayList<String> WhatYouPosts){
+        this.USERID  = userid;
+        this.USERNAME = username;
+        this. WhatYouPosts = fullname;
+    }
+    private void setMY_MESSAGES(ArrayList<Messages> xMsg){
+
+    }
+
+    public UAS1E(){}
+    public static void main(String[] args) {
+        UAS1E social = new UAS1E();
+        social.setUSERID("001");
+        social.setUSERNAME("@whitethat");
+        System.out.println("USERID :" + social.getUSERID());
+        System.out.println("USERNAME :" + social.getUSERNAME());
+        System.out.println("WHATYOUPOSTS :" + social.getFULLNAME());
+
+        int jum_msg=3;
+        Scanner UAS = new Scanner(System.in);
+        ArrayList<Messages> xMsg = new ArrayList<>();
+        for (int index=0; index<jum_msg;index++) {
+            Messages msg = new Messages();
+            msg.setUserID(social.getUSERID());
+            msg.setUserName(social.getUSERNAME());
+            System.out.println("Post Message" + String.valueOf(index + 1) + " =");
+            String _msg = UAS.nextLine(); 
+            msg.WhatYouPosts(_msg);
+            xMsg.add(msg);
+        }
+        social.setMY_MESSAGES(xMsg);
+    }
+
+ }
+
+ class Messages {
+    private String UserID;
+    private String UserName;
+    private ArrayList<Messages> FULLNAME;
+    private String WhatYouPosts;
+
+     public Messages(String userID, String userName, ArrayList<Messages> FULLNAME, String whatYouPosts) {
+         UserID = userID;
+         UserName = userName;
+         this.FULLNAME = FULLNAME;
+         WhatYouPosts = whatYouPosts;
+     }
+
+     public Messages(){
+
+     }
+
+     public String getUserID() {
+         return UserID;
+     }
+
+     public void setUserID(String userID) {
+         UserID = userID;
+     }
+
+     public String getUserName() {
+         return UserName;
+     }
+
+     public void setUserName(String userName) {
+         UserName = userName;
+     }
+
+     public ArrayList<Messages> getFULLNAME() {
+         return FULLNAME;
+     }
+
+     public void setFULLNAME(ArrayList<Messages> FULLNAME) {
+         this.FULLNAME = FULLNAME;
+     }
+
+     public String getWhatYouPosts() {
+         return WhatYouPosts;
+     }
+
+     public void setWhatYouPosts(String whatYouPosts) {
+         WhatYouPosts = whatYouPosts;
+     }
+
+     public void WhatYouPosts(String msg) {
+     }
+ }
+
 
 
 
